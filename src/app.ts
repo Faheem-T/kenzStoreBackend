f080883fe21386cee2078ed2e1acca4f1708093f
Faheem-T <faheemmbasheer@gmail.com>
2024-12-12 21:58:28 +0530

    initial setup

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import Express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = Express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is runnig on port ${port}`);
+});
