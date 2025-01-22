cebce9611a1340ae6971622b1988dd39214757ac
Faheem-T <faheemmbasheer@gmail.com>
2025-01-22 11:57:44 +0530

    add 'price' to cart type

--- src/types/cart.ts
+++ src/types/cart.ts
@@ -3,10 +3,31 @@ import { ProductType } from "./product";
 
 // SHARED TYPE: Sync with frontend
 export interface CartType {
+  _id: mongoose.Schema.Types.ObjectId;
   userId: mongoose.Schema.Types.ObjectId;
-  items: { productId: mongoose.Schema.Types.ObjectId; quantity: number }[];
+  items: {
+    _id: mongoose.Schema.Types.ObjectId;
+    productId: mongoose.Schema.Types.ObjectId;
+    price: number;
+    quantity: number;
+  }[];
 }
 // SHARED TYPE: Sync with frontend
 export interface ProductPopulatedCartType extends Omit<CartType, "items"> {
-  items: { productId: ProductType; quantity: number }[];
+  items: {
+    _id: mongoose.Schema.Types.ObjectId;
+    productId: Partial<ProductType>;
+    price: number;
+    quantity: number;
+  }[];
+}
+
+export interface PickProductPopulatedCartType<P extends keyof ProductType>
+  extends Omit<CartType, "items"> {
+  items: {
+    _id: mongoose.Schema.Types.ObjectId;
+    productId: Pick<ProductType, P>;
+    price: number;
+    quantity: number;
+  }[];
 }
